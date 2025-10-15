80607f85a3fa797a5cf85e0ab6e770c55a2f1e61
Helena Pedro <mbeua94@gmail.com>
2025-10-15 00:43:27 -0500

    lab10 updated

--- MPP-RESOURCES/labs/lab10/prob4/CountWords.java
+++ MPP-RESOURCES/labs/lab10/prob4/CountWords.java
@@ -4,32 +4,18 @@ import java.util.List;
 
 public class CountWords {
     public static void main(String[] args) {
-        CountWords cw = new CountWords();
-
-        int len = 4;
-        System.out.printf("Words that have length equal to 4: "
-                + cw.countWords(Folks.friends, 'N', 's', 4) + "%n");
+        CountWords wc = new CountWords();
+        int result = wc.countWords(Folks.friends, 'a', 'n', 4);
+        System.out.println("Number of words meeting criteria: " + result);
     }
 
     public int countWords(List<String> words, char c, char d, int len) {
-        // counts the number of words in the input list words that have length equal to len,
-        // that contain the character c, and that do not contain the character d.
+        //count how many words: have length equal to len, contain character c, do not contain character d
         return Math.toIntExact(words.stream()
                 .filter(s -> s.length() == len)
                 .filter(s -> s.indexOf(c) >= 0)
                 .filter(s -> s.indexOf(d) < 0)
                 .count()
         );
-//        int count = 0;
-//        for (String s : words) {
-//            if (s.length() == len) {
-//                for (int i = 0; i < s.length(); i++) {
-//                    if (s.charAt(i) != d && s.charAt(i) == c)
-//                        count++;
-//                }
-//            }
-//        }
-//        return count;
-
     }
 }
