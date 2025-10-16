5cc25883b861aa722cf96121381a2cbff57cbb9e
Helena Pedro <mbeua94@gmail.com>
2025-10-16 14:02:39 -0500

    lab11 updated

--- MPP-RESOURCES/labs/lab11/part1/prob2/Employee.java
+++ MPP-RESOURCES/labs/lab11/part1/prob2/Employee.java
@@ -1,32 +1,36 @@
 package labs.lab11.part1.prob2;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 
 public class Employee {
 	String name;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getSalary() {
-		return salary;
-	}
-	public void setSalary(int salary) {
-		this.salary = salary;
-	}
 	int salary;
+
 	public Employee(String n, int s) {
 		this.name = n;
 		this.salary = s;
 	}
+
 	@Override
 	public String toString() {
 		return "[" + name + ", " + salary+"]";
 	}
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getSalary() {
+        return salary;
+    }
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 	
 	public static void main(String[] args) {
 		List<Employee> list = new ArrayList<Employee>() {
@@ -41,9 +45,13 @@ public class Employee {
 				add(new Employee("Rich", 88000));
 			}
 		};
+        var sorted = list.stream()
+                .sorted(Comparator.comparing(Employee::getName)
+                        .thenComparing(Comparator.comparing(Employee::getSalary).reversed()))
+                .toList();
 		//expected output:
 		//[[Jim, 100000], [Jim, 75000], [Jim, 70000], [Joe, 59000], [Joe, 50000], [Rich, 88000], [Steve, 55000], [Tom, 80000]]
-		System.out.println(/*implement */);
+		System.out.println(sorted);
 	}
 }
 
